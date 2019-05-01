334c4a3120c5c447c30d4800a2bb58b9c1398c50
Rohit Singh <rohit.singh@rts.com.np>
2019-05-01 15:02:45 +0000

    fix lag and data corrupt issue

--- main/main.cpp
+++ main/main.cpp
@@ -12,7 +12,7 @@
 #include "esp_log.h"
 #include "driver/uart.h"
 #include "soc/uart_struct.h"
-#include "./RingBuffer.h"
+
 #include "../../rLibrary/rMqtt/mqtt.h"
 #include "../../rLibrary/rWifi/wifi.h"
 
@@ -21,27 +21,25 @@ extern "C" {
     #include "./ws2812_control.h"
 }
 
-#define TXD_PIN GPIO_NUM_4
-#define RXD_PIN GPIO_NUM_5
-
 static const char *TAG = "lightSwitch";
-static const int RX_BUF_SIZE = 1024;
 
 static const char * deviceUdi = "lightSwitch00001";
 static const char * devicePayloadType = "commandReply";
 static const uint16_t deviceThingCode = 13001;
 
-static RingBuffer* ringBuffer = new RingBuffer();
 static Mqtt* mqttInstance = new Mqtt();
 
 static char* PacketFormat = "{\"essential\":{\"subscriberudi\":\"%s\",\"payloadType\":\"%s\",\"payload\":{\"thingCode\":%d,\"data\":\"%s\"}}}";
 static uint16_t PacketLength = 0;
 
-void setColor(int color) {
-    struct led_state led_color;
-    for (int i = 0; i < 16; i++) {
-        led_color.leds[i] = color;
-    }
+void setColor(uint32_t color, uint32_t intensity) {
+    uint32_t red = (((0xFF0000 & color) >> 16) * intensity) / 255;
+    uint32_t green = (((0x00FF00 & color) >> 8) * intensity) / 255;
+    uint32_t blue = ((0x0000FF & color) * intensity) / 255;
+    uint32_t grb = (green << 16 | red << 8 | blue);
+    struct led_state led_color = {
+        grb, grb, grb, grb, grb, grb, grb, grb, grb, grb, grb, grb, grb, grb, grb, grb
+    };
     ws2812_write_leds(led_color);
 }
 
@@ -49,70 +47,58 @@ void subListener(esp_mqtt_event_handle_t event) {
     char * dataStream = (char *)calloc(event->data_len, sizeof(char *));
     memcpy(dataStream, event->data, event->data_len);
 
-    char * essential = (char *)calloc(128, sizeof(char *));
-    char * publisherudi = (char *)calloc(17, sizeof(char *));
-    char * payloadType = (char *)calloc(12, sizeof(char *));
-    char * payload = (char *)calloc(32, sizeof(char *));
+    struct json_token publisherudi = { NULL, 0, JSON_TYPE_INVALID };
+    struct json_token payloadType = { NULL, 0, JSON_TYPE_INVALID };
+    struct json_token payload = { NULL, 0, JSON_TYPE_INVALID };
+    struct json_token color = { NULL, 0, JSON_TYPE_INVALID };
     int thingCode = 0;
     int isChecked = 0;
     int intensity = 0;
-    char * color = (char *)calloc(128, sizeof(char *));
 
-    // PacketLength = strlen(PacketFormat) + strlen(deviceUdi) + strlen(devicePayloadType) + 512;
-    // char* ComposedPacket = (char*)calloc(PacketLength, sizeof(char *));
-    // int ComposedPacketLength = 0;
+    if (dataStream != NULL) {
 
-    // json_scanf(dataStream, event->data_len, "{ essential: %Q }", &essential);
-    json_scanf(dataStream, event->data_len, "{ publisherudi: %Q, payloadType: %Q, payload: %Q }", &publisherudi, &payloadType, &payload);
-    json_scanf(payload, strlen(payload), "{ thingCode: %d, isChecked: %B, intensity: %d, color: %Q}", &thingCode, &isChecked, &intensity, &color);
+        // PacketLength = strlen(PacketFormat) + strlen(deviceUdi) + strlen(devicePayloadType) + 512;
+        // char* ComposedPacket = (char*)calloc(PacketLength, sizeof(char *));
+        // int ComposedPacketLength = 0;
 
-    char parsedColor[6] = { 0 };
-    memcpy(parsedColor, color + 3, 6);
-    char correctedColor[6] = { parsedColor[2], parsedColor[3], parsedColor[0], parsedColor[1], parsedColor[4], parsedColor[5] };
-    int colorNumber = (int)strtol(correctedColor, NULL, 16);
-    printf("\ncolor in int: %X\n", colorNumber);
+        json_scanf(dataStream, event->data_len, "{ publisherudi: %T, payloadType: %T, payload: %T }", &publisherudi, &payloadType, &payload);
+        json_scanf(payload.ptr, payload.len, "{ thingCode: %d, isChecked: %B, intensity: %d, color: %T}", &thingCode, &isChecked, &intensity, &color);
 
-    if (isChecked) {
-        setColor(colorNumber);
-    } else {
-        setColor(0x00);
+        if (thingCode == 12001) {
+            int colorNumber = (int)strtol(color.ptr + 3, NULL, 16);
+            if (isChecked) {
+                setColor(colorNumber, intensity);
+            } else {
+                setColor(0x00, 0x00);
+            }
+            // printf("publisherudi: %.*s\npayloadType: %.*s\npayload: %.*s\n", publisherudi.len, publisherudi.ptr, payloadType.len, payloadType.ptr, payload.len, payload.ptr);
+            // printf("thingCode: %d\nisChecked: %d\nintensity: %d\ncolor: %.*s\n", thingCode, isChecked, intensity, color.len, color.ptr);
+        }
+
+        // ComposedPacketLength = snprintf(ComposedPacket, PacketLength, PacketFormat, deviceUdi, devicePayloadType, deviceThingCode, output);
+        // mqttInstance->Publish("RTSR&D/baanvak/sub/lightSwitch00001", ComposedPacket, ComposedPacketLength, 2, 0);
+
+        // free(ComposedPacket);
+        
+        free(dataStream);
     }
+}
 
-    // ComposedPacketLength = snprintf(ComposedPacket, PacketLength, PacketFormat, deviceUdi, devicePayloadType, deviceThingCode, output);
-    // mqttInstance->Publish("RTSR&D/baanvak/sub/lightSwitch00001", ComposedPacket, ComposedPacketLength, 2, 0);
-
-    // free(ComposedPacket);
-
-    // if (pin != 0 && (strcmp(action, (const char *)"high")) == 0) {
-    //     GPIOHigh(pin);
-    // }
-    // if (pin != 0 && (strcmp(action, (const char *)"low")) == 0) {
-    //     GPIOLow(pin);
-    // }
-    // if (pin != 0 && (strcmp(action, (const char *)"reset")) == 0) {
-    //     ResetCommand(pin);
-    // }
-    // if (pin != 0 && (strcmp(action, (const char *)"trigger")) == 0) {
-    //     TriggerCommand(pin);
-    // }
-    
-    printf("publisherudi: %s\npayloadType: %s\npayload: %s\n", publisherudi, payloadType, payload);
-    printf("thingCode: %d\nisChecked: %d\nintensity: %d\ncolor: %s\n", thingCode, isChecked, intensity, color);
-
-    free(dataStream);
-    free(essential);
-    free(publisherudi);
-    free(payloadType);
-    free(payload);
-    free(color);
+void mqttTask(void *) {
+    mqttInstance->Init();
+    mqttInstance->Connect();
+    *mqttInstance->Subscribe("RTSR&D/baanvak/sub/lightSwitch00001", 2) = subListener;
+    vTaskSuspend(NULL);
 }
 
 extern "C" void app_main() {
-    ESP_LOGI(TAG, "[APP] Startup..");
-    ESP_LOGI(TAG, "[APP] Free memory: %d bytes", esp_get_free_heap_size());
-    ESP_LOGI(TAG, "[APP] IDF version: %s", esp_get_idf_version());
+    // ESP_LOGI(TAG, "[APP] Startup..");
+    // ESP_LOGI(TAG, "[APP] Free memory: %d bytes", esp_get_free_heap_size());
+    // ESP_LOGI(TAG, "[APP] IDF version: %s", esp_get_idf_version());
 
     nvs_flash_init();
+
+    ws2812_control_init();
 
     Wifi wifiInstance = Wifi();
     wifiInstance.Init();
@@ -120,11 +106,8 @@ extern "C" void app_main() {
 
     mqttInstance->Init();
     mqttInstance->Connect();
-
     *mqttInstance->Subscribe("RTSR&D/baanvak/sub/lightSwitch00001", 2) = subListener;
 
-    ws2812_control_init();
-
+    // xTaskCreatePinnedToCore(mqttTask, (const char*)"MqttTask", 20480, NULL, 2, NULL, 1);
     // xTaskCreate(receiveTask, (const char*)"Receive Task", 2048, (void *)1, 1, NULL);
-    // xTaskCreate(runner, (const char*)"runner", 2048, (void *)1, 1, NULL);
 }
